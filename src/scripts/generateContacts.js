3ff7adbdf2bb54428865b67e262dd016a14220a8
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-05-28 17:20:09 -0600

    upd

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,7 @@
 import { PATH_DB } from '../constants/contacts.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+console.log(PATH_DB);
+};
 
 await generateContacts(5);
